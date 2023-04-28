70f060b8ee2b0e6f16aca09409475cd65c5f05f2
Thomman3825 <tkurian2@icloud.com>
2023-04-28 10:24:58 +0530

    shapes printed

---
+++ Practice/FileName.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class Account
+    {
+
+        /*public int salary { get; set; }*/
+        public int debits { get; set; }
+        public int credits { get; set; }
+
+    }
+    class program
+    {
+        Account a1 = new Account();
+         static int salary;
+        
+       /* public static void Main()
+        {
+            Console.WriteLine("Spending Tracker");
+            Console.WriteLine("1.Enter Salary");
+            Console.WriteLine("2.Enter Spendings");
+            Console.WriteLine("3.Enter Earnings");
+            Console.WriteLine("4.View Summary");
+            Console.WriteLine("5.Exit");
+            Console.Write("Enter your choice: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+
+        }*/
+    }
+}
---
+++ Practice/Shapes.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class Shapes
+    {
+        public static void Main()
+        {
+            while (true) { 
+            Console.WriteLine("Printing Shapes");
+            Console.WriteLine("1.Equilateral Triangle");
+            Console.WriteLine("2.Sqaure");
+            Console.WriteLine("3.Right Angle Triangle");
+            Console.WriteLine("4.Exit");
+            Console.WriteLine("Enter your Choice:");
+            
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        printEquilateralTriangle();
+                        break;
+                    case 2:
+                        printSquare();
+                        break;
+                    case 3:
+                        printRightAngleTriangle();
+                        break;
+                        case 4:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Enter another choice.");
+                            break;
+                }
+            }
+        }
+
+        public static void printEquilateralTriangle()
+        {
+            Console.WriteLine("Enter no of rows:");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            int count = rows - 1;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < count; j++)
+                {
+                    Console.Write(" ");
+                    count--;
+                }
+                for (int j= 0; j <= 2 * i - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+        public static void printSquare()
+        {
+            Console.WriteLine("Enter no of rows:");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0;j < rows; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine() ;
+            }
+
+        }
+
+        public static void printRightAngleTriangle()
+        {
+            Console.WriteLine("Enter no of rows:");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 0; i <= rows; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine() ;
+            }
+        }
+    }
+}
